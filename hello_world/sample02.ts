755db41d7d92d7ade0ce6848743dd208713e526a
ando <leo81@outlook.jp>
2024-02-13 19:10:35 +0900

    sample code追加

--- hello_world/sample02.ts
+++ hello_world/sample02.ts
@@ -1,16 +1,23 @@
 // 配列
 const nums: number[] = [10, 20, 30];
 const strs: string[] = ["foo", "bar", "hoge"];
-console.log(nums[0]) // 10
-console.log(strs[0]) // foo
+console.log(nums); // [ 10, 20, 30 ]
+console.log(strs); // [ 'foo', 'bar', 'hoge' ]
 
 // 配列のコピー (参照)
 var nums2 = nums; // numsと同じオブジェクトを参照
 nums2[0] = 99;
-console.log(nums[0]) // 99
+console.log(nums); // [ 99, 20, 30 ]
 
-// 配列のコピー (値)
+// 配列のコピー (値) (スプレッド構文を使った方法)
 // 値を入れる場合はスプレッド構文を使用してコピーする  (スプレッド構文は「...」(ドット３つ)を前につける)
 nums2 = [...nums]; // nums配列の値をコピー
 nums2[0] = 100;
-console.log(nums[0]) // 99
+console.log(nums); // [ 99, 20, 30 ]
+
+// 配列のコピー (値) (concatを使った方法)
+nums2 = nums.concat(); // nums配列の値をコピー
+nums2[0] = 101;
+console.log(nums); // [ 99, 20, 30 ]
+
+console.log(nums2); // [ 101, 20, 30 ]
